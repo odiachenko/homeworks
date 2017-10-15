1bf40371e8e526819da3243ff32240309eb0b5e6
odiachenko <32524348+odiachenko@users.noreply.github.com>
2017-10-15 13:39:11 +0300

    code style

--- lesson2/main.c
+++ lesson2/main.c
@@ -4,63 +4,50 @@
 int main()
 {
     printf("Enter number:\n");
-    int rows;
+    int rows = 0;
 
     scanf("%d", &rows);
     printf("you entered %d\n", rows);
-    //tree1(rows);
-    //tree2(rows);
-    tree3(rows);
-    return 0;
-}
-
-void tree1(int rows)
-{
-    int i;
-    int j;
-    for ( i = 0; i < rows; i++) {
-        for ( j = 0; j <= i; j++) {
-                printf("*");
-        }
-        printf("\n");}
-}
-
-void tree2(int rows)
-{
-    int i;
-    int j;
-    for ( i = rows; i > 0; i--) {
-        for ( j = 0; j < i; j++) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int z = 0;
+    int m = 0;
+    // Вывести треугольник;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j <= i; j++) {
             printf("*");
         }
-        printf("\n");}
-}
+        printf("\n");
+    }
+    printf("\n");
 
-void tree3(int rows)
+    // Вывести перевернутый треугольник;
+    for (i = rows; i > 0; i--) {
+        for (j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
 
-{
-    int i;
-    int j;
-    int k;
-    int z;
-    int m;
+    // Нарисовать елочку;
     for ( i = 0; i < rows; i++) {
-            for (k = 0; k < (rows - i); k++){
-            printf (" ");}
+        for (k = 0; k < (rows - i); k++) {
+            printf(" ");
+        }
+        for (j = 0; j <= i; j++) {
+            printf("*");
+        }
+        for (z = 1; z < j; z++){
+                    printf("*");
+        }
+        printf("\n");
+    }
+    for (i = 0; i < rows; i++) {
+        printf(" ");
+    }
+    printf("*");
 
-                for ( j = 0; j <= i; j++) {
-                printf ("*");}
-
-                    for (z = 1; z < j; z++){
-                    printf ("*");}
-
-             printf("\n");}
-                        for ( i = 0; i < rows; i++) {
-                        printf (" ");}
-
-                        printf ("*");
+    return 0;
 }
-
-
-
-
