2894542849107f26d3e7217bd9c589772d050a8f
odiachenko <32524348+odiachenko@users.noreply.github.com>
2017-10-15 13:41:12 +0300

    added check for input words; changed question for exit

--- lesson3/main.c
+++ lesson3/main.c
@@ -6,49 +6,56 @@ int main()
 {
     int searchedNum = 0;
     int usersNum = 0;
-    int selector = 0;
+    char selector;
     int i = 0;
 
-        for (;;){
+    for (;;) {
         srand(time(NULL));
         searchedNum = rand()% 10;
+        i = 0;
 
-            for (i = 0; i < 3; i++){
-                //printf("%d-try");
-            }
-                for (;;){
-                    printf(" Enter positive value: ");
-                    scanf("%d", &usersNum);
-                    if (usersNum < 0){
-                    printf(" Hey! It's negative num!\n");
-                    continue;
-                  }
+        for (;;){
+            if (i == 3) {
                 break;
-                }
+            }
+            printf(" Enter positive value: ");
+            if (!scanf("%d", &usersNum)) {
+                printf(" Hey! It's not num!\n");
+                cleanBuffer();
+                continue;
+            }
 
-                    if (usersNum == searchedNum){
-                    printf("\nYahoo! RIGHT!");
+            if (usersNum < 0){
+                printf(" Hey! It's negative num!\n");
+                continue;
+            }
+            i++;
+            printf("%d-try\n", i);
 
-        break;
-
-        if (usersNum < searchedNum){
-        printf("Bigger!");
+            if (usersNum == searchedNum){
+                printf("\nYahoo! RIGHT!");
+                //break;
+                return 0;
+            }
+            if (usersNum < searchedNum){
+                printf("Bigger!\n");
+            }
+            if (usersNum > searchedNum){
+                printf("Lower!\n");
+            }
         }
 
+        printf("\nContinue or exit? yes or no\n");
+        scanf("\n%c", &selector);
+        cleanBuffer();
 
-        if (usersNum > searchedNum){
-        printf("Lower");
+        if (selector == 'n'){
+            return 0;
         }
-        printf("\nContinue or exit? 1\0");
-        scanf("%d", &selector);
-
-        if (selector == 0){
+    }
     return 0;
-        }
-                    }
-        }
 }
 
- //return 0;
-
-
+void cleanBuffer(){
+    while (getchar()!='\n');
+}
