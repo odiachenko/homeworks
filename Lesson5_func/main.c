d21cf07c04fb29579bd78cb816e5afd16bce8d40
odiachenko <32524348+odiachenko@users.noreply.github.com>
2017-11-11 18:27:27 +0200

    Print only characters

--- Lesson5_func/main.c
+++ Lesson5_func/main.c
@@ -4,20 +4,17 @@
 const int MAX_CHARACTER = 10000;
 const int ASCII_SIZE = 256;
 
-void initArray(char myArray[][2]);
-void sortArray(char myArray[][2]);
-void printArray(char myArray[][2]);
+void initArray(char sourceArray[][2]);
+void sortArray(char sourceArray[][2]);
+void printArray(char sourceArray[][2]);
 
-int main()
-{
-    int size = 256;
-
-    char characterArr[MAX_CHARACTER];
+int main() {
+    char characterArray[MAX_CHARACTER];
     char insertionsForSort[ASCII_SIZE][2];
 
     printf("Enter some string!\n");
 
-    fgets(characterArr, MAX_CHARACTER, stdin);
+    fgets(characterArray, MAX_CHARACTER, stdin);
 
     // Initialization array with ASCII codes
     initArray(insertionsForSort);
@@ -25,8 +22,8 @@ int main()
     // calculate insertions
     int i = 0;
     int k = 0;
-    while (characterArr[i] != '\0') {
-        int charCode = characterArr[i];
+    while (characterArray[i] != '\0') {
+        int charCode = characterArray[i];
         for (k = 0; k < ASCII_SIZE; k++) {
             if (insertionsForSort[k][0] == charCode) {
                 insertionsForSort[k][1]++;
@@ -44,48 +41,47 @@ int main()
     return 0;
 }
 
- void initArray(char myArray[][2])
- {
+ void initArray(char sourceArray[][2]) {
     int k = 0;
     const int asciiStart = -128;
     for (k = 0; k < ASCII_SIZE; k++) {
-        myArray[k][0] = asciiStart + k;
-        myArray[k][1] = 0;
+        sourceArray[k][0] = asciiStart + k;
+        sourceArray[k][1] = 0;
     }
  }
 
- void sortArray(char myArray[][2])
- {
+ void sortArray(char sourceArray[][2]) {
     int i = 0;
     int k = 0;
     for (k = 0; k < ASCII_SIZE; k++) {
         for (i = k + 1; i < ASCII_SIZE; i++) {
-            if (myArray[k][1] < myArray[i][1]) {
-                int tmp[2] = {myArray[k][0], myArray[k][1]};
-                myArray[k][0] = myArray[i][0];
-                myArray[k][1] = myArray[i][1];
-                myArray[i][0] = tmp[0];
-                myArray[i][1] = tmp[1];
+            if (sourceArray[k][1] < sourceArray[i][1]) {
+                int tmp[2] = {sourceArray[k][0], sourceArray[k][1]};
+                sourceArray[k][0] = sourceArray[i][0];
+                sourceArray[k][1] = sourceArray[i][1];
+                sourceArray[i][0] = tmp[0];
+                sourceArray[i][1] = tmp[1];
             }
         }
     }
  }
 
- void printArray(char myArray[][2])
- {
-     int k = 0;
+ void printArray(char sourceArray[][2]) {
+    int k = 0;
     for (k = 0; k < ASCII_SIZE; k++) {
-        if (myArray[k][1] != 0) {
-            if (myArray[k][0] == 32) { // check for space
-                printf("insertions of space is %d\n", myArray[k][1]);
-                continue;
+        int charCode = sourceArray[k][0];
+        if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+            if (sourceArray[k][1] != 0) {
+                /*if (sourceArray[k][0] == 32) { // check for space
+                    printf("insertions of space is %d\n", sourceArray[k][1]);
+                    continue;
+                }
+                if (sourceArray[k][0] == 10) { // check for new line
+                    printf("insertions of \\n is %d\n", sourceArray[k][1]);
+                    continue;
+                }*/
+                printf("insertions of %c is %d\n", sourceArray[k][0], sourceArray[k][1]);
             }
-            if (myArray[k][0] == 10) { // check for new line
-                printf("insertions of \\n is %d\n", myArray[k][1]);
-                continue;
-            }
-            printf("insertions of %c is %d\n", myArray[k][0], myArray[k][1]);
         }
     }
  }
-
